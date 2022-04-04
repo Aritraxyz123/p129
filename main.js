33c045b5b383fe4749d78057b1c4348c1fbdaeec
Aritraxyz123 <98028589+Aritraxyz123@users.noreply.github.com>
2022-04-04 20:00:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+song1="";
+song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+scoreLeftWrist=0;
+song1Status="";
+
+function preload(){
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
+}
+function setup(){
+    canvas=createCanvas(600, 500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('posenet is initialised');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX=" + leftWristX + " leftWristY=" + leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX=" + rightWristX + " rightWristY=" + rightWristY);
+    }
+}
+function draw(){   
+fill("#FF0000");
+stroke("FF0000");
+song1Status=song1.isPlaying();
+if(scoreLeftWrist > 0.2){
+    circle(leftWristX, leftWristY, 20);
+    song2.stop();
+}
+if(song1Status==false){
+    song1.play();
+    document.getElementById("song").innerHTML="playing-peterpansong"
+}
+}
